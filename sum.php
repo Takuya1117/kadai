a7e0d586b4da70f2d5ed7a07c967a0dd85aa75c4
oosugi takuya <gct1123213@icloud.com>
2021-05-21 13:22:35 +0000

    kadai04

--- sum.php
+++ sum.php
@@ -4,30 +4,41 @@ function sum($max) {
  return $result* 2;
 }
 echo sum(10);
+echo "\n";
+echo "\n";
 
 //kadai2
 function add($a,$b){
 return $a +$b;
 }
-echo plus(3,7);
+echo add(3,7);
+echo "\n";
+echo "\n";
 
 //kadai3
 function number($arr){
-  $result = 0;
+    $result = 1;
 
-    for($i = 1; $i <= count($arr); $i++ );
-        $result *= $arr[$i];
-return $result;  
+    foreach($arr as $ar){
+        $result *= $ar;
+        //$result = $result * $ar;
+    }
+    return $result; 
 }
+
 echo number(array(1,3,5,7,9,));
 
 //kadai4
 function max_array($arr){
-// とりあえず配列の最初の要素を一番大きい値とする
- $max_number = $arr[0];
- foreach($arr as $a){
- //どうしたらいいかわからない・・・・
- }
+    // とりあえず配列の最初の要素を一番大きい値とする
+    $max_number = $arr[0];
+    foreach($arr as $a){
+    //どうしたらいいかわからない・・・・
+        if($max_number < $a){
+            $max_number = $a;
+        }
+    }
+    return $max_number;
+}
 
- return $max_number;
- }
+echo max_array(array(1,3,5,7,9,));
